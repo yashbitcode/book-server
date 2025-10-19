342ace756d593ff4222767609f22eb53cf9077dc
yashbitcode <yashkumar.sre.177@gmail.com>
2025-10-19 13:34:05 +0530

    postgres connection and controller updated

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,77 +1,73 @@
-const { boooks } = require("../models");
+const { booksTable } = require("../models/index");
+const db = require("../db/index");
+const { eq } = require("drizzle-orm");
 
-const getAllBooks = (req, res) => {
-    res.json(books);
+const getAllBooks = async (req, res) => {
+    const data = await db.select().from(booksTable);
+    return res.json(data);
 };
 
-const getBookById = (req, res) => {
-    const id = parseInt(req.params.id);
+const getBookById = async (req, res) => {
+    const id = req.params.id;
 
-    if (isNaN(id))
-        return res.status(400).json({
-            error: "Id should be a type number",
-        });
+    try {
+        const book = await db
+            .select()
+            .from(booksTable)
+            .where(eq(booksTable.id, id));
 
-    const book = books.find((el) => el.id == id);
-
-    if (!book)
+        return res.json(book);
+    } catch {
         return res.status(404).send({
             error: `Book doesn't exist with the id ${id}`,
         });
-
-    return res.json(book);
+    }
 };
 
-const createBook = (req, res) => {
-    const { title, author } = req.body;
+const createBook = async (req, res) => {
+    const { title, authorId, description } = req.body;
 
-    if (!title)
-        return res.status(400).json({
-            error: "title required",
+    try {
+        if (!title)
+            return res.status(400).json({
+                error: "title required",
+            });
+        
+        const [result] = await db.insert(booksTable).values({
+            title,
+            authorId,
+            description
+        }).returning({
+            id: booksTable.id
         });
 
-    if (!author)
-        return res.status(400).json({
-            error: "author required",
+        return res.status(201).json({
+            message: "Book created Successfully",
+            id: result.id,
         });
-
-    const id = Date.now();
-
-    books.push({
-        id,
-        title,
-        author,
-    });
-
-    return res.status(201).json({
-        message: "Book created Successfully",
-        id,
-    });
+    } catch {
+        res.status(400).json({
+            error: "Author ID not valid",
+        });
+    }
 };
 
-const deleteBookById = (req, res) => {
-    const id = parseInt(req.params.id);
+const deleteBookById = async (req, res) => {
+    const id = req.params.id;
 
-    if (isNaN(id))
-        return res.status(400).json({
-            error: "Id should be a type number",
-        });
-
-    const bookIdx = books.findIndex((el) => el.id == id);
-
-    if (bookIdx < 0)
+    try {
+        await db.delete(booksTable).where(eq(booksTable.id, id));
+        return res.json({ message: "Deletion Successful", id });
+    } catch {
         return res.status(404).send({
             error: `Book doesn't exist with the id ${id}`,
         });
-
-    books.splice(bookIdx, 1);
-
-    return res.json({ message: "Deletion Successful", id });
+    }
 };
 
 module.exports = {
     getAllBooks,
     getBookById,
     createBook,
-    deleteBookById
+    deleteBookById,
 };
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv/config");
 const express = require("express");
 const { logger } = require("./middlewares/loggerMiddleware");
-
+const booksTable = require("./models/book.model");
+const db = require("./db/index");
 const bookRouter = require("./routes/books.route");
 
 const app = express();
@@ -9,23 +11,23 @@ const PORT = 8000;
 app.use(express.json());
 app.use(logger);
 
-// app.get("/", (req, res) => res.send("Hi Bro!!"));
-// app.use("/books", bookRouter);
+app.get("/", (req, res) => res.send("Hi Bro!!"));
+app.use("/books", bookRouter);
 
-app.use((req, res, next) => {
-    req.time = Date.now();
-    next();
-});
+// app.use((req, res, next) => {
+//     req.time = Date.now();
+//     next();
+// });
 
-app.get("/", (req, res, next) => {
-    req.val = 1221;
-    next();
-}, (req, res, next) => {
-    console.log(`${req.time}, ${req.val}`);
-    next();
-});
+// app.get("/", (req, res, next) => {
+//     req.val = 1221;
+//     next();
+// }, (req, res, next) => {
+//     console.log(`${req.time}, ${req.val}`);
+//     next();
+// });
 
-app.get("/", (req, res) => res.send("hi"));
+// app.get("/", (req, res) => res.send("hi"));
 
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
